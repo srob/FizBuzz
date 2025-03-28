d2df4638b3af9543958d14db15387432e643fdda
srob <sjroberts@gmail.com>
2025-03-28 14:49:17 +0000

    Update FizzBuzz.java - fixed failing test again

--- src/main/java/com/scrumcenter/csd/FizzBuzz.java
+++ src/main/java/com/scrumcenter/csd/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
     public static String fizzbuzz(int n) {
         if (n % 3 == 0 && n % 5 == 0) return "FizzBuzz";
         if (n % 3 == 0) return "Fizz";
-        if (n % 7 == 0) return "Buzz";
+        if (n % 5 == 0) return "Buzz";
         return String.valueOf(n);
     }
 }
